05024ee60d854d5bf6bf8c058ac3ede6ace82af1
Andres Buitrago <Andresjavier87@hotmail.com>
2023-12-05 18:31:10 -0500

    Primer commit - Agregar proyecto Spring

---
+++ src/test/java/com/group/hotelApi/HotelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.hotelApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

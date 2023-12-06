744a421205c1f4aa9d2967f9937c381af5151f89
hector868 <hectormancilla489@gmail.com>
2023-12-05 21:33:57 -0600

    segundo commit de hector

--- src/main/java/com/group/hotelApi/controller/HelloController.java
+++ src/main/java/com/group/hotelApi/controller/HelloController.java
@@ -1,7 +1,6 @@
 package com.group.hotelApi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,11 +11,6 @@ public class HelloController {
     @GetMapping
     public String helloWorld(){
         return "Hello World!!";
-    }
-    
-    @GetMapping("/hector")
-    public String helloWorld2() {
-    	return "Hello world 2";
     }
 
 }

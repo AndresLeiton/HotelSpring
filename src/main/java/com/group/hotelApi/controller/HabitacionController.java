ece5d064e4356f3c737b834f500d1f1f992b7b79
hector868 <hectormancilla489@gmail.com>
2023-12-05 23:09:13 -0600

    Controller v1

--- src/main/java/com/group/hotelApi/controller/HabitacionController.java
+++ src/main/java/com/group/hotelApi/controller/HabitacionController.java
@@ -1,5 +1,6 @@
 package com.group.hotelApi.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/habitacion")
 public class HabitacionController {
 
+	@GetMapping
+	public void envio() {
+		System.out.println("Verb HTTP");
+	}
 }
